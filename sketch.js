180924bf818a8ac703ce692dbcaccd5c4981df34
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-11-20 15:04:03 +0100

    Added p5 skeleton

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// canvas vars
+var width;
+var height;
+
+function setup() {
+  width = 600;
+  height = 400;
+  createCanvas(width, height);
+  background(50);
+}
+
+function draw() {
+  background(50);
+}
